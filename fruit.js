3054e138bd62a244260588b7b4472ecf49e18278
Santiago Parini <santiago.parini@globallogic.com>
2018-04-03 16:24:28 -0300

    fruit class

---
+++ fruit.js
@@ -0,0 +1,31 @@
+class Fruit{
+	constructor(cols,rows,size){
+		this.r = 0
+		this.g = 255
+		this.b = 0
+		this.rows = rows
+		this.cols = cols
+		this.v = createVector(floor(random(0,cols)) * size,floor(random(0,rows)) * size)
+		this.size = size
+	}
+
+	show(){
+		fill(this.r,this.g,this.b)
+		rect(this.v.x,this.v.y,this.size,this.size)
+	}
+
+	reset(snake){
+		this.v.x = floor(random(0,this.cols)) * this.size
+		this.v.y = floor(random(0,this.rows)) * this.size
+		for(var i = 0; i<snake.length;i++){
+			if(snake[i].x == this.v.x && snake[i].y == this.v.y){
+				this.reset(snake)
+			} 
+		}
+		return this.pos()
+	}
+
+	pos(){
+		return this.v
+	}
+}
